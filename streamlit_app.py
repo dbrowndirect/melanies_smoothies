3e4d4fc66c360fae34cbb3ba143046a851faf9ab
dbrowndirect <dbrown@otip.com>
2026-08-25 14:36:45 -0400

    Clean up commented code in streamlit_app.py
    Removed commented-out code for fruit selection and dataframe display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,12 +14,6 @@ st.write(
   """
 )
 
-# option = st.selectbox(
-#    "What is your favorite fruit?",
-#    ("Banana", "Strawberries", "Peaches"),
-#)
-
-# st.write("Your favorite fruit is:", option)
 NAME_ON_ORDER = ""
 NAME_ON_ORDER = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", NAME_ON_ORDER)
@@ -32,8 +26,7 @@ session = cnx.session()
 # SiS: session = get_active_session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
-# st.stop()
+
 # Convert the Snowpark Datagram to a Pandas Dataframe so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
 # st.dataframe(pd_df)
@@ -50,7 +43,7 @@ if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
-        ingredients_string +=fruit_chosen + ' '
+        ingredients_string += fruit_chosen + ' '
 
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
