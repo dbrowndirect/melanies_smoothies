a4df949eb96d00719d31acd7d85e523a190d11a6
dbrowndirect <dbrown@otip.com>
2026-08-25 13:48:48 -0400

    Fetch and display smoothie nutrition information
    Updated the app to fetch and display smoothie nutrition information from the Smoothiefroot API.

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 import os
 from snowflake.snowpark.functions import col
 # Sis: from snowflake.snowpark.context import get_active_session
-  
+import requests  
 
 
 # Write directly to the app
@@ -63,10 +63,10 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-        
-        # New section to display smoothiefroot nutrition information
-        import requests
+
+    ingredients_string = ''
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-        # st.text(smoothiefroot_response.json())
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
