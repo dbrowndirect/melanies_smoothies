b7d5b59ae3e7530d87a6db26f60f81892a627318
dbrowndirect <dbrown@otip.com>
2026-08-25 14:33:17 -0400

    Improve ingredient selection and order submission
    Refactor ingredient handling and submission logic in the Streamlit app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,32 +46,11 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-    # st.write("You selected:", ingredients_list)
-    # st.text(ingredients_list)
-
+    
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string +=fruit_chosen + ' '
-
-    # st.write(ingredients_string)
-
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-                    values ('""" + ingredients_string + """','"""+NAME_ON_ORDER+ """')"""
-
-    # st.write(my_insert_stmt)
-    # st.stop()
-    
-    time_to_insert = st.button('Submit Order')
-    
-    # if ingredients_string:
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered!', icon="✅")
-
-    ingredients_string = ''
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
 
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
@@ -80,3 +59,15 @@ if ingredients_list:
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
+
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+                    values ('""" + ingredients_string + """','"""+NAME_ON_ORDER+ """')"""
+
+    time_to_insert = st.button('Submit Order')
+    
+    # if ingredients_string:
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon="✅")
+
+        
